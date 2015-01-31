5b62e9654f6d026d25de6e536ffca1665cf501fc
Joshua T Corbin <joshua@uber.com>
2015-01-30 22:10:38 -0800

    TChannel: use prototype-less peers and endpoints collections

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ function TChannel(options) {
 	this.timeoutCheckInterval = this.options.timeoutCheckInterval || 1000;
 	this.timeoutFuzz = this.options.timeoutFuzz || 100;
 
-	this.peers = {};
+	this.peers = Object.create(null);
 
-	this.endpoints = {};
+	this.endpoints = Object.create(null);
 	this.destroyed = false;
 
 	this.serverSocket = new net.createServer();
